2d78b9b22ef8b54b2939a2d871584804a66e2f78
Pixi iMac1 <PixiiMac01@piximac1.local>
2019-04-05 12:00:21 +0530

    Test

---
+++ jsonCodeable/jsonCodeable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jsonCodeable
+//
+//  Created by PIXI IMAC 01 on 05/04/2019.
+//  Copyright © 2019 Pixi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
